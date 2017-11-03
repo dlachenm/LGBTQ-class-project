63f9a84d9b38689920b5b6c0b3042d8fc41b0793
gaydrianna <33139296+gaydrianna@users.noreply.github.com>
2017-11-03 14:19:57 -0400

    structure of code

---
+++ structure for data from cdc.py
@@ -0,0 +1,25 @@
+#objective_1= isolate question in survey ASL220_00.000 male How do you think of yourself (sexual orientation) multiple choice 2016
+#objective_2 = isolate question in survey ASL240_00.000 female How do you think of yourself (sexual orientation) multiple choice 2016
+#overall objective, repeat for each survey from 2016, 2014 and compare results for bisexual choice in sexual orientation question for male and female
+#check if question is the same for 2014 in code and placement. repeat for that dataset as well
+import csv
+
+f = open("")
+csv_f = csv.reader(f)
+#determine male or female based on column_x sex question 1 for male, 2 for female
+for all in csv_f:
+    acisim =
+#next step: find column that corresponds with ASL220_00.000
+    
+    
+#step_3: determine female
+    acisif =
+    
+#step_4 find column that correspons with ASL240_00.000
+
+
+#import another dataset
+    
+#do same shit
+    
+#write to a json file of just those datasets from larger national survey 
