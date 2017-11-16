4a75e05452f7f4c0db025953f82183b82cc298db
gaydrianna <33139296+gaydrianna@users.noreply.github.com>
2017-11-16 18:55:43 -0500

    added some step two code

--- structure for data from cdc.py
+++ structure for data from cdc.py
@@ -4,16 +4,33 @@
 #check if question is the same for 2014 in code and placement. repeat for that dataset as well
 import csv
 
-f = open("")
+f = open("samadult_2016.csv")
 csv_f = csv.reader(f)
-#determine male or female based on column_x sex question 1 for male, 2 for female
+#determine male or female based on column_x sex question 1 for male, 2 for female #g collum equals [6] x
 for all in csv_f:
-    acisim =
-#next step: find column that corresponds with ASL220_00.000
+    sex_code = all[6]
+   # question_male = all[]
+    #question_female = all[]
     
+    try:
+        if int(sex_code) <= 1:
+            print(sex_code, "male") 
+        #add write command to write out the record of males and their responses to the sex orientation question 
+        
+        else:
+            print(sex_code, "female")
+            #add write command to write out the record of females and their responses to the sex orientation question
+            
+    except:
+        print(sex_code, "nonbinary")
+        
+f.close()
+#next step: find column that corresponds with ASL220_00.000
+    #asisim male sex orientation wz
+    #asisif female sex orientation xa 
     
 #step_3: determine female
-    acisif =
+    #acisif =
     
 #step_4 find column that correspons with ASL240_00.000
 
