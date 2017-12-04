9650a5fd3e1a9afda99dcd8466efe0498dec14e5
Elana Scaglia <elanascaglia@Elana-Scaglias-MacBook-2.local>
2017-12-04 08:17:54 -0500

    counter
    Posture Magazine date counter for the number of articles published per year 2013-2017

--- Posture_art.py
+++ Posture_art.py
@@ -20,6 +20,7 @@ for counter in range (1,9):
     page_html = LGBTQ_art.text
 
     soup = BeautifulSoup(page_html, "html.parser")
+        #dates = soup.find_all (re.compile('\w([0-9])')
     #print(page_html)
 
     #articles = []
@@ -29,14 +30,12 @@ for counter in range (1,9):
 
     for a_article in LGBTQ_art_articles:
         #print ('---------')
-        #print(a_article)
         #print(LGBTQ_art_articles.text)
 
 
         a_title = a_article.find ('h3')
         #print('-----------')
         print(a_title.text)
-        #it wont print the .text its not clear why this happens, it worked in another of my scripts
         #print(a_title['href'])
 
 
@@ -66,6 +65,18 @@ for counter in range (1,9):
         }
         all_data.append(an_article)
 
+        #filter(lambda year: date['a_date.text']=='\w([0-9])', an_article)
+#What i want now: the date separated out as just the year, and be able to count the number of articles per year
+
+        #reobj = re.compile('date')
+        #for year in an_article.keys():
+            #if(reobj.match(year)):
+                #print (year, an_article[year])
 print (all_data)
 with open('all_data.json', 'w') as posturemag:
     posturemag.write(json.dumps(all_data, indent=4))
+    #dates = json.find_all (re.compile('\w([0-9])'), posturemag)
+    #print (dates)
+
+# NOW PULL NUMBER OF ARTICLES PER YEAR
+#regular expression for dates \w([0-9])
